e0e626f17d99770b224d914b52efc8b0a28eafa5
Pedro Figueiredo <pedrofigsesteves@gmail.com>
2023-01-28 20:09:50 -0300

    lógica js finalizada

---
+++ js/script.js
@@ -0,0 +1,126 @@
+const addressForm = document.querySelector("#address-form");
+const cepInput = document.querySelector("#cep");
+const addressInput = document.querySelector("#address");
+const cityInput = document.querySelector("#city");
+const neighborhoodInput = document.querySelector("#neighborhood");
+const regionInput = document.querySelector("#region");
+const formInputs = document.querySelectorAll("[data-input]");
+
+const fadeElement = document.querySelector("#fade");
+
+const closeButton = document.querySelector("#close-message");
+
+// Validate CEP Input
+cepInput.addEventListener("keypress", (e) => {
+  const onlyNumbers = /[0-9]/;
+  const key = String.fromCharCode(e.keyCode);
+
+  if (!onlyNumbers.test(key)) {
+    e.preventDefault();
+    return;
+  }
+});
+
+
+
+//get customer address from api
+
+const getAddress = async (cep) => {
+  toggleLoader();
+
+  cepInput.blur();
+
+  const apiUrl = `https://viacep.com.br/ws/${cep}/json/`;
+
+  const response = await fetch(apiUrl);
+
+  const data = await response.json();
+
+  //show error and reset form
+  if (data.erro === true) {
+
+    if(!addressInput.hasAttribute('disabled')){
+        toggleDisabled()
+    }
+
+    addressForm.reset();
+    toggleLoader();
+    toggleMessage(`CEP inválido. Tente novamente!`);
+    return;
+  }
+
+  if(addressInput.value === ''){
+    toggleDisabled()
+  }
+
+  addressInput.value = data.logradouro;
+  cityInput.value = data.localidade;
+  neighborhoodInput.value = data.bairro;
+  regionInput.value = data.uf;
+
+  toggleLoader();
+};
+
+//show or hide loader
+const toggleLoader = () => {
+  const loaderElement = document.querySelector("#loader");
+
+  fadeElement.classList.toggle("hide");
+  loaderElement.classList.toggle("hide");
+};
+
+const toggleMessage = (msg) => {
+  const messageElement = document.querySelector("#message");
+  const messageElementText = document.querySelector("#message p");
+
+  messageElementText.innerText = msg;
+
+  fadeElement.classList.toggle("hide");
+  messageElement.classList.toggle("hide");
+};
+
+//add or remove disabled attribute
+const toggleDisabled = () => {
+
+    if(regionInput.hasAttribute('disabled')){
+        formInputs.forEach((input) => {
+            input.removeAttribute('disabled')
+        })
+    }else{
+        formInputs.forEach((input) => {
+            input.setAttribute('disabled', 'disabled')
+        })
+    }
+}
+
+//EVENTS
+
+//get address event
+cepInput.addEventListener("keyup", (e) => {
+    const inputValue = e.target.value;
+  
+    //check if we have a CEP
+    if (inputValue.length === 8) getAddress(inputValue);
+  });
+
+//close message modal
+closeButton.addEventListener('click', (e) => toggleMessage())
+
+//save address
+addressForm.addEventListener('submit', (e) => {
+
+    e.preventDefault()
+    
+    toggleLoader()
+
+    setTimeout(() => {
+
+        toggleLoader()
+
+        toggleMessage('Endereço salvo com sucesso!')
+
+        addressForm.reset()
+        
+        toggleDisabled()
+    }, 3000);
+})
